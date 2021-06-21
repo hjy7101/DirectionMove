37c50d0b52a9890ef9a78668ff1bfa390c628693
hktrollgames <hktrollgames@gmail.com>
2021-06-21 18:15:36 +0900

    Attack레이어 만들어서 이동과 공격 모션이 같이 되게함

--- Assets/DirectionMove.cs
+++ Assets/DirectionMove.cs
@@ -9,6 +9,7 @@ public class DirectionMove : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        lastMoveDirection = transform.forward;
     }
 
     // Update is called once per frame
@@ -20,14 +21,53 @@ public class DirectionMove : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) move.z = 1;
         if (Input.GetKey(KeyCode.S)) move.z = -1;
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            state = StateType.Attack;
+            animator.Play("Attack", 1);
+        }
+
         if (move != Vector3.zero)
         {
             move.Normalize();
             transform.Translate(move * 5 * Time.deltaTime
                 , Space.World);
 
-            transform.forward = move;
+            //transform.forward = move; // 이코드 있으면 작동안함.
+            lastMoveDirection = move;
+            state = StateType.Run;
+            animator.Play("Run");
         }
+        else
+        {
+            move = lastMoveDirection;
+            state = StateType.Idle;
+            animator.Play("Idle");
+        }
+
+        transform.forward = Vector3.Slerp(transform.forward
+            , move, roateLerp);
+
+        //transform.forward = transform.forward; // roateLerp ==0; 오른쪽,
+        //transform.forward = move; // roateLerp == 1;
+
+        //roateLerp: 0.05
+        //transform.forward : 앞쪽,100
+        //move; 오른쪽(0)
+        //    // 1frame : 95
+        //    // 2frame : 91
+        //    // 3frame : 87
+        //    // 4frame : 84
+        //    // 5frame : 83
+        //    // 60 frame : 0
     }
     public float speed = 5;
+    public float roateLerp = 0.5f;
+    public Vector3 lastMoveDirection;
+
+    public StateType state = StateType.Idle;
+    public enum StateType
+    {
+        Idle, Run, Jump, Attack,
+    }
 }
